bb8a0b01fc7ef85528f8383ba7a1c4ce92a30906
Alex <at1468@york.ac.uk>
2021-01-28 11:20:26 +0000

    testing eclipse git

--- src/survivalapp/SurvivalApp.java
+++ src/survivalapp/SurvivalApp.java
@@ -1,11 +1,9 @@
 package survivalapp;
 
-import java.io.*;
-
 public class SurvivalApp {
 
     public static void main(String[] args) {
 
-        System.out.print("Hello World!");
+        System.out.print("Hello World from eclipse");
     }
 }
