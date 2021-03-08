9b9d0c6c806ff2c561854a2560f7e086e85e29ed
at1468 <at1468@york.ac.uk>
2021-03-08 13:12:06 +0000

    Added empty methods

--- src/main/java/parser/SlideshowHandler.java
+++ src/main/java/parser/SlideshowHandler.java
@@ -1,7 +1,34 @@
 package parser;
 
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class SlideshowHandler extends DefaultHandler {
 
+    //TODO attriubtes for media elements and slides once classes are written
+
+    private String elementValue;
+
+    @Override
+    public void characters(char [] chars, int start, int length) throws SAXException {
+        elementValue = new String(chars, start, length);
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        //TODO create new slideshow
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        //TODO handle start elements
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        //TODO handle end elements
+    }
+
+    //TODO getter for finished slideshow
 }
