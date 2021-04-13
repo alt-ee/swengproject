ef5ea0e40d9c8e0b6446b6a0a942a406409310ec
rn775 <rn775@york.ac.uk>
2021-04-13 18:31:54 +0100

    Set up text handler

--- src/main/java/texthandler/Text.java
+++ src/main/java/texthandler/Text.java
@@ -25,26 +25,61 @@ public class Text
     }
 
     //setter
-    public void setWrittenText(String text) { this.writtenText = text; }
+    public void setWrittenText(String text)
+    {
+        this.writtenText = text;
+    }
 
-    //append text
-    public void appendText(String newWrittenText) { writtenText += newWrittenText; } //append new text to existing text
-    public void appendBoldText(String newWrittenText) { writtenText += "<b>" + newWrittenText+ "</b>"; } //append new BOLD text to existing text
-    public void appendItalicText(String newWrittenText) { writtenText += "<i>" + newWrittenText + "</i>"; } //append new ITALIC text to existing text
+    //append new text to existing text
+    public void appendText(String newWrittenText)
+    {
+        writtenText += newWrittenText;
+    }
 
+    //append new BOLD text to existing text
+    public void appendBoldText(String newWrittenText)
+    {
+        writtenText += "<b>" + newWrittenText+ "</b>";
+    }
+
+    //append new ITALIC text to existing text
+    public void appendItalicText(String newWrittenText)
+    {
+        writtenText += "<i>" + newWrittenText + "</i>";
+    }
 
     //Getters for required parameters
-    public int getTextXPos() { return textXPos; } //Returns X-position of text
+    public int getTextXPos() {
+        return textXPos;
+    } //Returns X-position of text
 
-    public int getTextYPos() { return textYPos; } //Returns Y-position of text
+    public int getTextYPos()
+    {
+        return textYPos;
+    } //Returns Y-position of text
 
-    public String getWrittenText() { return writtenText; } //Returns written text as String
+    public String getWrittenText()
+    {
+        return writtenText;
+    } //Returns written text as String
 
-    public String getTextFont() { return textFont; } //Returns the text font as String
+    public String getTextFont()
+    {
+        return textFont;
+    } //Returns the text font as String
 
-    public int getTextFontSize() { return textFontSize; } //Returns font size of text
+    public int getTextFontSize()
+    {
+        return textFontSize;
+    } //Returns font size of text
 
-    public Color getTextColour() { return textColour; } //Returns colour of text
+    public Color getTextColour()
+    {
+        return textColour;
+    } //Returns colour of text
 
-    public int getTextDuration() { return textDuration; } //Returns the duration text will be present in seconds
+    public int getTextDuration()
+    {
+        return textDuration;
+    } //Returns the duration text will be present in seconds
 }
