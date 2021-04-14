8d72350e435658fafda5f4a57c118923914e03ab
at1468 <59831659+at1468@users.noreply.github.com>
2021-04-14 16:32:48 +0100

    removed unnecessary malformed url test

--- src/test/java/parser/SlideshowHandlerTest.java
+++ src/test/java/parser/SlideshowHandlerTest.java
@@ -54,52 +54,6 @@ class SlideshowHandlerTest {
     }
 
     @Test
-    void testMalformedURLs() throws Exception {
-
-        BufferedInputStream bufferedInputStream = getBufferedInputStreamFromFilepath("src/test/resources/malformed_url_test.xml");
-
-        Slideshow parsedSlideshow;
-        Slideshow expectedSlideshow = new Slideshow();
-
-        Color expectedBackgroundColour = Color.decode("#faf347");
-        String expectedFont = "Calibri";
-        int expectedFontSize = 16;
-        Color expectedTextColour = Color.decode("#855797");
-        Color expectedLineColour = Color.decode("#fa44dd");
-        Color expectedShapeColour = Color.decode("#88cd02");
-
-        expectedSlideshow.setDefaults(expectedBackgroundColour, expectedFont, expectedFontSize, expectedTextColour, expectedLineColour, expectedShapeColour);
-
-        SlideDataStorage goodAudioSlide = new SlideDataStorage("goodAudio", 0);
-        SlideDataStorage goodVideoSlide = new SlideDataStorage("goodVideo", 0);
-        SlideDataStorage goodImageSlide = new SlideDataStorage("goodImage", 0);
-        SlideDataStorage badAudioSlide = new SlideDataStorage("badAudio", 0);
-        SlideDataStorage badVideoSlide = new SlideDataStorage("badVideo", 0);
-        SlideDataStorage badImageSlide = new SlideDataStorage("badImage", 0);
-
-        URL audioURL =  new URL("file://bird.wav");
-        goodAudioSlide.addAudio(new AudioDataStorage(audioURL, 0, false));
-
-        URL videoURL = new URL("file://bird.mov");
-        goodVideoSlide.addVideo(new VideoDataStorage(100, 100, videoURL, 0, true));
-
-        URL imageURL = new URL("file://bird.png");
-        goodImageSlide.addImage(new ImageDataStorage(100, 100, imageURL, 10, 10, 0));
-
-        expectedSlideshow.addSlide(goodAudioSlide);
-        expectedSlideshow.addSlide(badAudioSlide);
-        expectedSlideshow.addSlide(goodVideoSlide);
-        expectedSlideshow.addSlide(badVideoSlide);
-        expectedSlideshow.addSlide(goodImageSlide);
-        expectedSlideshow.addSlide(badImageSlide);
-
-        parser.parse(bufferedInputStream, handler);
-        parsedSlideshow = handler.getSlideshow();
-
-        assertEquals(parsedSlideshow, expectedSlideshow);
-    }
-
-    @Test
     void testDefaults() throws Exception{
 
         BufferedInputStream bufferedInputStream = getBufferedInputStreamFromFilepath("src/test/resources/defaults_test.xml");
