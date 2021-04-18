494dc316026fcb707f967013febf90dd40f51d00
at1468 <59831659+at1468@users.noreply.github.com>
2021-04-18 09:47:00 +0100

    Wrote test for standard expected handler behaviour of valid xml file

--- src/test/java/parser/SlideshowHandlerTest.java
+++ src/test/java/parser/SlideshowHandlerTest.java
@@ -1,13 +1,14 @@
 package parser;
 
 import datastorage.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXParseException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.awt.*;
 import java.io.*;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,9 +37,7 @@ class SlideshowHandlerTest {
 
         BufferedInputStream bufferedInputStream = getBufferedInputStreamFromFilepath("src/test/resources/not_an_xml_file.txt");
 
-        Assertions.assertThrows(SAXParseException.class, () -> {
-            parser.parse(bufferedInputStream, handler);
-        });
+        assertThrows(SAXParseException.class, () -> parser.parse(bufferedInputStream, handler));
     }
 
     @Test
@@ -46,9 +45,7 @@ class SlideshowHandlerTest {
 
         BufferedInputStream bufferedInputStream = getBufferedInputStreamFromFilepath( "src/test/resources/invalid_xml_file.xml");
 
-        Assertions.assertThrows(SAXParseException.class, () -> {
-            parser.parse(bufferedInputStream, handler);
-        });
+        assertThrows(SAXParseException.class, () -> parser.parse(bufferedInputStream, handler));
     }
 
     @Test
@@ -99,7 +96,83 @@ class SlideshowHandlerTest {
 
         BufferedInputStream bufferedInputStream = getBufferedInputStreamFromFilepath("src/test/resources/unexpected_element.xml");
 
-        Assertions.assertDoesNotThrow(() -> parser.parse(bufferedInputStream, handler));
-        Assertions.assertDoesNotThrow(() -> handler.getSlideshow());
+        assertDoesNotThrow(() -> parser.parse(bufferedInputStream, handler));
+        assertDoesNotThrow(() -> handler.getSlideshow());
     }
+
+    @Test
+    void testValidFile() throws Exception {
+
+        BufferedInputStream bufferedInputStream = getBufferedInputStreamFromFilepath("src/test/resources/PWS_example.xml");
+
+        Slideshow parsedSlideshow;
+
+        // Constructing a SlideshowHandler that matches the desired output of the provided XML file
+
+        Slideshow expectedSlideshow = new Slideshow();
+
+        expectedSlideshow.setDefaults(Color.decode("#43675a"),
+                "Calibri",
+                16,
+                Color.decode("#a6aeae"),
+                Color.decode("#f2c95e"),
+                Color.decode("#6dcbdb"));
+
+        SlideDataStorage slide1 = new SlideDataStorage("slide01", 0);
+        TextDataStorage text1 = new TextDataStorage(100,
+                200,
+                "<html>test text <b>some bold text</b> <i>some italic text</i> <b>some more bold text</b> some normal text</html>",
+                expectedSlideshow.getDefaultFont(),
+                expectedSlideshow.getDefaultFontSize(),
+                expectedSlideshow.getDefaultTextColour(),
+                0);
+        LineDataStorage line1 = new LineDataStorage(40, 20, 80, 20,
+                expectedSlideshow.getDefaultLineColour(),
+                0);
+        ShapeDataStorage shape1 = new ShapeDataStorage(10, 10, 20 ,34,
+                expectedSlideshow.getDefaultShapeColour(),
+                10,
+                ShapeDataStorage.Shapes.Oval);
+        ShaderDataStorage shader1 = new ShaderDataStorage(0, 0, 0, 0,
+                Color.decode("#f2a54b"),
+                Color.decode("#dd452a"),
+                false);
+        shape1.setShader(shader1);
+        TextDataStorage text2 = new TextDataStorage(7,
+                34,
+                "<html>more test text with specified font</html>",
+                "Joker",
+                expectedSlideshow.getDefaultFontSize(),
+                expectedSlideshow.getDefaultTextColour(),
+                0);
+        ImageDataStorage image1 = new ImageDataStorage(10, 10,
+                new URL("file://resources/images/image.png"),
+                5,
+                6,
+                0);
+
+        slide1.addText(text1);
+        slide1.addText(text2);
+        slide1.addLine(line1);
+        slide1.addShape(shape1);
+        slide1.addImage(image1);
+
+        SlideDataStorage slide2 = new SlideDataStorage("slide02", 100);
+
+        VideoDataStorage video1 = new VideoDataStorage(20, 9,
+                new URL("file://resources/videos/video.mp4"), "testVideo", true);
+        AudioDataStorage audio1 = new AudioDataStorage(new URL("file://resources/audio/audio.wav"), 10, true);
+
+        slide2.addVideo(video1);
+        slide2.addAudio(audio1);
+
+        expectedSlideshow.addSlide(slide1);
+        expectedSlideshow.addSlide(slide2);
+
+        parser.parse(bufferedInputStream, handler);
+        parsedSlideshow = handler.getSlideshow();
+
+        assertEquals(expectedSlideshow, parsedSlideshow);
+    }
+
 }
