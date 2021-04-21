80df72e0df968984145d692e1d84c7ab7a3da976
Andrew <at1475@york.ac.uk>
2021-04-21 18:18:57 +0100

    Added a test for unusually pressing play multiple times & for Stop with no file loaded.

--- src/test/java/audiohandler/AudioPlayerTest.java
+++ src/test/java/audiohandler/AudioPlayerTest.java
@@ -25,9 +25,16 @@ class AudioPlayerTest {
         }
 
     @Test
-    void noFileLoaded() {
+    void noFileLoadedPlay() {
         AudioPlayer myPlayer = new AudioPlayer();
         myPlayer.playClip(false);
+        assertNull(myPlayer.audioClip);
+    }
+
+    @Test
+    void noFileLoadedStop() {
+        AudioPlayer myPlayer = new AudioPlayer();
+        myPlayer.stopClip();
         assertNull(myPlayer.audioClip);
     }
 
@@ -83,6 +90,38 @@ class AudioPlayerTest {
                 counter += 1;
             }
             if (counter == 3000000){
+                myPlayer.playClip(false);
+            }
+            counter += 1;
+        }
+    }
+
+    @Test
+    void unusualInteraction() {
+        AudioPlayer myPlayer = new AudioPlayer();
+        String input = "src/main/resources/RedKite1.wav";
+        try {
+            myPlayer.loadClip(input);
+        } catch (IOException e) {
+            System.out.println("File not found");
+        } catch (UnsupportedAudioFileException e) {
+            System.out.println("File is of incorrect format");
+        } catch (LineUnavailableException e) {
+            System.out.println("Line unavailable");
+        }
+        long clipLength = myPlayer.audioClip.getMicrosecondLength()/1000;
+        myPlayer.playClip(false);
+
+        long counter = 0;
+        while (myPlayer.audioClip.getMicrosecondPosition()/1000 < clipLength) {
+            if (clipLength == myPlayer.audioClip.getMicrosecondPosition()) {
+                assertEquals(clipLength, myPlayer.audioClip.getMicrosecondPosition());
+            }
+            if (counter == 2000000){
+                myPlayer.playClip(false);
+                counter += 1;
+            }
+            if (counter == 5000000){
                 myPlayer.playClip(false);
             }
             counter += 1;
