e79eae048287832b3a70611cf336b821c940ea56
rn775 <rn775@york.ac.uk>
2021-04-25 20:47:34 +0100

    refactor addText method and rewrite timer using swing

--- src/main/java/texthandler/TextTest.java
+++ src/main/java/texthandler/TextTest.java
@@ -2,12 +2,14 @@ package texthandler;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Timer;
-import java.util.TimerTask;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TextTest
 {
     WriteText write = new WriteText();
+    JFrame frame = new JFrame();         //example JFrame to test
 
     // 4 different JPanels, to test 4 different JLabels
     JPanel panel = new JPanel();         //Test 1st JPanel
@@ -15,11 +17,9 @@ public class TextTest
     JPanel panel3 = new JPanel();        //Test 3rd JPanel
     JPanel panelDuration = new JPanel(); //Test 4th Duration JPanel
 
-    public TextTest()
+    public void textTest()
     {
-        //example JFrame to test
         //Frame onto which the JPanel will be shown
-        JFrame frame = new JFrame();
         frame.setVisible(true);
         frame.setSize(1000, 600);
         frame.setTitle("Text Test");
@@ -27,6 +27,7 @@ public class TextTest
         frame.setLayout(null); //null for setBounds to work
 
         /***********************************************************************/
+
         //1st panel to TEST
         panel.setSize(210, 190);    //set the height and width of JPanel
         panel.setLocation(240, 250);      //set the X-position and Y-position of the JPanel
@@ -34,9 +35,9 @@ public class TextTest
         panel.setLayout(null);                  //set layout at null to manually input where text is drawn in panel
 
         //Adding TEXT onto JPanel
-        write.addText(panel, 30, 100, "PlainText", "Serif", 40, Color.RED, 5000);
-        write.addText(panel, 30, 25, "plain text", "Serif", 20, Color.RED, 0);
-        write.addText(panel, 45, 65, "plain text again", "Serif", 20, Color.RED, 0);
+        write.addText(panel, 30, 100, "Test 1", "Serif", 40, Color.RED, 5000);
+        write.addText(panel, 30, 25, "Test 2", "Serif", 20, Color.RED, 0);
+        write.addText(panel, 45, 65, "Test 3", "Serif", 20, Color.RED, 6000);
 
         /***********************************************************************/
         //2nd panel to TEST
@@ -46,8 +47,8 @@ public class TextTest
         panel2.setLayout(null);
 
         //Adding TEXT onto JPanel
-        write.addText(panel2, 20, 60, "Bold Text", "Calibri", 30, Color.BLACK, 0);
-        write.addText(panel2, 10, 25, "bold test", "Arial", 20, Color.BLACK, 0);
+        write.addText(panel2, 20, 60, "Test 4", "Calibri", 30, Color.BLACK, 7500);
+        write.addText(panel2, 10, 25, "Test 5", "Arial", 20, Color.BLACK, 0);
 
         /***********************************************************************/
         //3rd panel to TEST
@@ -57,8 +58,8 @@ public class TextTest
         panel3.setLayout(null);
 
         //Adding TEXT onto JPanel
-        write.addText(panel3, 10, 75, "Italic Text", "Arial", 30, Color.BLUE, 0);
-        write.addText(panel3, 50, 15, "test italic", "Calibri", 40, Color.BLUE, 0);
+        write.addText(panel3, 10, 75, "Test 6", "Arial", 30, Color.BLUE, 0);
+        write.addText(panel3, 50, 15, "Test 7", "Calibri", 40, Color.BLUE, 5500);
 
         /***********************************************************************/
         //4th duration panel to TEST
@@ -70,35 +71,32 @@ public class TextTest
         //Duration Test, text:"test" should disappear one-by-one
         for (int i = 50; i < 950; i += 50)
         {
-            write.addText(panelDuration, i, 20, "test", "Serif", 15, Color.BLACK, i*10);
+            write.addText(panelDuration, i, 20, "test", "Serif", 15, Color.BLACK, i * 10);
+            //duration: 500, 1000, 1500, 2000, 2500.....
         }
-
-        //Example of using an external timer so that text will be displayed only for its specified duration
-        int period = 500;    //How often the timer task will be called in milliseconds
-
-        //Instantiate Timer and TimerTask objects from Java.util
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask()
-        {
-            //Specify the behaviour of the timer task utilised by the timer
-            @Override
-            public void run()
-            {
-                //Increment the WriteText's stored time value by the period of the timer
-                write.incrementCurrentTime(period);
-                System.out.println("Current panel time: " + write.getCurrentTime() + " milliseconds");
-                //Refresh the test JPanel so changes can be seen
-                panelDuration.repaint();
-                panelDuration.revalidate();
-            }
-        };
-
-        //Schedule timer to run the timer task with no initial delay every 500 milliseconds (half a second)
-        timer.scheduleAtFixedRate(task, 0, period);
     }
 
     public static void main(String[] args)
     {
-        new TextTest(); //Entry point
+        TextTest test = new TextTest(); //Entry point
+
+        //Example of using an external timer so that text will be displayed only for its specified duration
+        int period = 500;    //How often the timer task will be called in milliseconds
+
+        ActionListener al = new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                //Increment the WriteText's stored time value by the period of the timer
+                test.write.incrementCurrentTime(period);
+                System.out.println("\nCurrent panel time: " + test.write.getCurrentTime() + " milliseconds");
+                test.textTest();
+            }
+        };
+
+        //Timer to run the action listener every 500 milliseconds (half a second)
+        Timer timer = new Timer(period, al);
+        timer.start();
     }
 }
--- src/main/java/texthandler/WriteText.java
+++ src/main/java/texthandler/WriteText.java
@@ -5,8 +5,7 @@ import java.awt.*;
 
 public class WriteText
 {
-    //JLabels as array, so multiple labels of the same type are all added to JPanel
-    JLabel[] label = new JLabel[20];
+    JLabel label = new JLabel();
 
     //The current time in milliseconds that an external timer has been running for
     int currentTime = 0;
@@ -24,19 +23,30 @@ public class WriteText
     //draws text on JPanel
     public void addText(JPanel panel, int XPos, int YPos, String text, String font, int fontSize, Color colour, int duration)
     {
+
         //Draw text if duration=0 or duration set is more than the currentTime
         if (duration == 0 || duration >= currentTime)
         {
-            for (int i = 0; i < 20; i++)
-            {
-                //panel.setBackground(Color.PINK);                                //make JPanel Pink for identification during TESTING
-                label[i] = new JLabel();                                        //create new JLabels for each text added
-                label[i].setBounds(XPos, YPos, fontSize * 10, fontSize);  //determines x & y bound positions for text
-                label[i].setFont(new Font(font, Font.PLAIN, fontSize));         //determines font and font size of text
-                label[i].setForeground(colour);                                 //changes text colour
-                label[i].setText(text);                                         //writes text on panel
-                panel.add(label[i]);                                            //add JLabel onto JPanel
-            }
+            //System.out.print(" IN"); //Test
+            //panel.setBackground(Color.YELLOW);                           //make JPanel Pink for identification during TESTING
+            label = new JLabel();                                        //create new JLabels for each text added
+            label.setBounds(XPos, YPos, fontSize * 10, fontSize);  //determines x & y bound positions for text
+            label.setFont(new Font(font, Font.PLAIN, fontSize));         //determines font and font size of text
+            label.setForeground(colour);                                 //changes text colour
+            label.setText(text);                                         //writes text on panel
+            panel.add(label);                                            //add JLabel onto JPanel
         }
+        else
+        {
+            //System.out.print(" OUT"); //Test
+            panel.setBackground(Color.PINK);
+            panel.remove(label);
+            //label.setVisible(false);
+            //label.setForeground(Color.CYAN);
+        }
+
+        //Refresh the JPanel so changes can be seen
+        panel.repaint();
+        panel.revalidate();
     }
 }
