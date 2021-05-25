8380ff11c1475bcad36ab61e8eaf4258c45b0f36
at1468 <59831659+at1468@users.noreply.github.com>
2021-05-25 14:21:26 +0100

    Wrote code to pass test

--- src/main/java/survivalapp/View.java
+++ src/main/java/survivalapp/View.java
@@ -1,10 +1,13 @@
 package survivalapp;
 
+import imagehandler.ImageType;
+
 import javax.swing.*;
 
 public class View {
 
     private JFrame window;
+    private JPanel panel;
 
     public void newWindow(int width, int height) {
         window = new JFrame();
@@ -12,5 +15,16 @@ public class View {
         window.setSize(width, height);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+
+        window.add(panel);
+    }
+
+    public void drawImage(int xPos, int yPos, String url, int width, int height, int duration) {
+
+        ImageType image = new ImageType(url, xPos, yPos, width, height, panel);
+        image.drawImageWithScale();
+        window.repaint();
     }
 }
