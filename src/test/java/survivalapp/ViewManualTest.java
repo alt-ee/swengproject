2b1454d071152d9d06b785b400ea3ae7acd391ad
at1468 <59831659+at1468@users.noreply.github.com>
2021-05-25 12:48:34 +0100

    Wrote window test

---
+++ src/test/java/survivalapp/ViewManualTest.java
@@ -0,0 +1,11 @@
+package survivalapp;
+
+public class ViewManualTest {
+
+    public static void main(String[] args) {
+
+        View view = new View();
+
+        view.newWindow(1280, 720);
+    }
+}
