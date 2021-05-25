72f9267302521faeab5a3f86f7b3d6f65c2d2b2f
at1468 <59831659+at1468@users.noreply.github.com>
2021-05-25 13:57:36 +0100

    Wrote test for drawImage

--- src/test/java/survivalapp/ViewManualTest.java
+++ src/test/java/survivalapp/ViewManualTest.java
@@ -1,5 +1,9 @@
 package survivalapp;
 
+import datastorage.ImageDataStorage;
+
+import java.net.URL;
+
 public class ViewManualTest {
 
     public static void main(String[] args) {
@@ -7,5 +11,24 @@ public class ViewManualTest {
         View view = new View();
 
         view.newWindow(1280, 720);
+
+
+        int xPos1 = 20;
+        int yPos1 = 20;
+        String fileLocation1 = "/src/test/resources/image0.jpg";
+        int width1 = 100;
+        int height1 = 100;
+        int duration1 = 0;
+
+        view.drawImage(xPos1, yPos1, fileLocation1, width1, height1, duration1);
+
+        int xPos2 = 20;
+        int yPos2 = 200;
+        String fileLocation2 = "/src/test/resources/hulk.JPEG";
+        int width2 = 100;
+        int height2 = 100;
+        int duration2 = 0;
+
+        view.drawImage(xPos2, yPos2, fileLocation2, width2, height2, duration2);
     }
 }
