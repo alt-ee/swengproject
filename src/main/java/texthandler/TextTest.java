1be3712bb43a5f53dc9250cea4340f2e3ca0ab19
rn775 <rn775@york.ac.uk>
2021-05-27 21:54:54 +0100

    working duration using removeAll() to clear panel

--- src/main/java/texthandler/TextTest.java
+++ src/main/java/texthandler/TextTest.java
@@ -19,27 +19,27 @@ public class TextTest
         frame.setSize(1000, 600);
         frame.setTitle("Text Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null); //null for setBounds to work
+        frame.setLayout(null);                   //null for setBounds to work
 
         //panel to TEST
         panel.setSize(900, 500);    //set the height and width of JPanel
         panel.setLocation(50, 50);        //set the X-position and Y-position of the JPanel
         frame.add(panel);                       //add JPanel onto JFrame
         panel.setLayout(null);                  //set layout at null to manually input where text is drawn in panel
-        panel.setBackground(Color.PINK);        //Test background colour
-        //panel.setOpaque(false);
     }
 
     public void demoTest()
     {
+        panel.removeAll(); // remove all previous labels before adding new ones
+
         //Adding TEXT onto JPanel
         write.addText(panel, 300, 100, "Text 1", "Serif", 40, Color.RED, 4500);
-        write.addText(panel, 300, 200, "Text 2", "Serif", 20, Color.RED, 6500);
-        write.addText(panel, 150, 300, "Text 3", "Serif", 20, Color.RED, 8500);
-        write.addText(panel, 100, 170, "Text 4", "Arial", 30, Color.BLUE, 10500);
+        write.addText(panel, 400, 200, "Text 2", "Serif", 20, Color.RED, 6500);
+        write.addText(panel, 650, 300, "Text 3", "Serif", 20, Color.RED, 8500);
+        write.addText(panel, 200, 370, "Text 4", "Arial", 30, Color.BLUE, 10500);
 
-        write.addText(panel, 200, 150, "Text 5", "Calibri", 30, Color.BLACK, 0);
-        write.addText(panel, 100, 350, "Text 6", "Arial", 20, Color.BLACK, 0);
+        write.addText(panel, 200, 225, "Text 5", "Calibri", 30, Color.BLACK, 0);
+        write.addText(panel, 700, 400, "Text 6", "Arial", 20, Color.BLACK, 0);
         write.addText(panel, 500, 150, "Text 7", "Calibri", 40, Color.BLUE, 0);
 
         //Duration Test, text:"test" should disappear one-by-one
--- src/main/java/texthandler/WriteText.java
+++ src/main/java/texthandler/WriteText.java
@@ -22,27 +22,24 @@ public class WriteText
     //draws text on JPanel
     public void addText(JPanel panel, int XPos, int YPos, String text, String font, int fontSize, Color colour, int duration)
     {
-        JLabel label = new JLabel();    // new JLabel for every time a new text is drawn
-        //panel.remove(label);            // removes previous label, so labels aren't stacked on top of each other
+        JLabel label = new JLabel();
 
-        if ((duration == 0 || duration >= currentTime)) //Draw text if duration=0 or duration set is more than the currentTime
+        panel.remove(label);                                           //remove previous label so labels aren't stacked
+        label.setBounds(XPos, YPos, fontSize * 10, fontSize);    //determines x & y bound positions for text
+        label.setFont(new Font(font, Font.PLAIN, fontSize));           //determines font and font size of text
+        label.setForeground(colour);                                   //changes text colour
+        label.setText(text);                                           //writes text on panel
+
+        //Draw text if duration=0 or duration set is more than the currentTime
+        if ((duration == 0 || duration >= currentTime))
         {
-            //panel.setOpaque(false);
-            label.setBounds(XPos, YPos, fontSize*10, fontSize);    //determines x & y bound positions for text
-            label.setFont(new Font(font, Font.PLAIN, fontSize));         //determines font and font size of text
-            label.setForeground(colour);                                 //changes text colour
-            label.setText(text);                                         //writes text on panel
-            panel.add(label);                                            //add JLabel onto JPanel
+            panel.add(label);                                          //add JLabel onto JPanel
             System.out.println(" IN: " + label.getText()); //Test
         }
         else
         {
-            System.out.println(" OUT: " + label.getText()); //Test -- label does not go through 'else' statement
-            //panel.setOpaque(false);
-            //label.setOpaque(false);
-            //label.setText(".");
-            //label.setForeground(Color.LIGHT_GRAY);
-            panel.remove(label); //does not work as JLabels do not go in 'else' statement correctly, indicated by the print statements
+            panel.remove(label);
+            System.out.println(" OUT: " + label.getText()); //Test
         }
 
         //Refresh the JPanel so changes can be seen
