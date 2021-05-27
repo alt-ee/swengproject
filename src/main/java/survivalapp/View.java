eb1dfe0eb4c08caccc5c1d3a54bb2299aa73d177
at1468 <59831659+at1468@users.noreply.github.com>
2021-05-27 16:09:08 +0100

    Wrote drawShape to pass test

--- src/main/java/survivalapp/View.java
+++ src/main/java/survivalapp/View.java
@@ -1,6 +1,9 @@
 package survivalapp;
 
 import datastorage.ImageDataStorage;
+import datastorage.ShaderDataStorage;
+import datastorage.ShapeDataStorage;
+import graphicshandler.GraphicsPanel;
 import imagehandler.ImageType;
 
 import javax.swing.*;
@@ -8,19 +11,23 @@ import javax.swing.*;
 public class View {
 
     private JFrame window;
-    private JPanel panel;
+    private GraphicsPanel panel;
 
     public void newWindow(int width, int height) {
         window = new JFrame();
 
         window.setSize(width, height);
-        window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        panel = new JPanel();
+        window.setVisible(true);
+        panel = new GraphicsPanel();
 
         window.add(panel);
     }
+
+    public void repaintPanel() {
+        panel.repaint();
+    }
+
 
     /***
      * Wrapper method for image handler to use our image type
@@ -30,7 +37,6 @@ public class View {
     public void drawImage(ImageDataStorage image) {
 
         String url = image.getFileLocation().getFile().substring(1);
-        System.out.println(url);
         int xPos = image.getXPos();
         int yPos = image.getYPos();
         int width = image.getWidth();
@@ -38,6 +44,39 @@ public class View {
 
         ImageType imageType = new ImageType(url, xPos, yPos, width, height, panel);
         imageType.drawImageWithScale();
-        window.repaint();
+    }
+
+    public void drawShape(ShapeDataStorage shape) {
+
+        int xPos = shape.getxPosition();
+        int yPos = shape.getyPosition();
+        int width = shape.getWidth();
+        int height = shape.getHeight();
+        String colour = shape.getColourInHex();
+        ShaderDataStorage shader = shape.getShader();
+        int duration = shape.getDuration();
+
+        if (shader != null) {
+            int shaderX1 = shader.getX1();
+            int shaderY1 = shader.getY1();
+            int shaderX2 = shader.getX2();
+            int shaderY2 = shader.getY2();
+            String shaderColour1 = shader.getColour1InHex();
+            System.out.println(shaderColour1);
+            String shaderColour2 = shader.getColour2InHex();
+            boolean cyclic = shader.isCyclic();
+
+            if (shape.isOval()) {
+                panel.addShadedOval(height, width, xPos, yPos, shaderX1, shaderY1, shaderColour1, shaderX2, shaderY2, shaderColour2, cyclic, duration);
+            } else if (shape.isRectangle()) {
+                panel.addShadedRect(height, width, xPos, yPos, shaderX1, shaderY1, shaderColour1, shaderX2, shaderY2, shaderColour2, cyclic, duration);
+            }
+        } else {
+            if (shape.isOval()) {
+                panel.addOval(height, width, xPos, yPos, colour, true, duration);
+            } else if (shape.isRectangle()) {
+                panel.addRect(height, width, xPos, yPos, colour, true, duration);
+            }
+        }
     }
 }
