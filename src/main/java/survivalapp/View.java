2b8abcfc4aa0a633f3323a59b4622c1e273c08b7
at1468 <59831659+at1468@users.noreply.github.com>
2021-05-27 11:06:45 +0100

    Rewrote drawImage to pass new test

--- src/main/java/survivalapp/View.java
+++ src/main/java/survivalapp/View.java
@@ -1,5 +1,6 @@
 package survivalapp;
 
+import datastorage.ImageDataStorage;
 import imagehandler.ImageType;
 
 import javax.swing.*;
@@ -21,10 +22,22 @@ public class View {
         window.add(panel);
     }
 
-    public void drawImage(int xPos, int yPos, String url, int width, int height, int duration) {
+    /***
+     * Wrapper method for image handler to use our image type
+     *
+     * @param image
+     */
+    public void drawImage(ImageDataStorage image) {
 
-        ImageType image = new ImageType(url, xPos, yPos, width, height, panel);
-        image.drawImageWithScale();
+        String url = image.getFileLocation().getFile().substring(1);
+        System.out.println(url);
+        int xPos = image.getXPos();
+        int yPos = image.getYPos();
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        ImageType imageType = new ImageType(url, xPos, yPos, width, height, panel);
+        imageType.drawImageWithScale();
         window.repaint();
     }
 }
