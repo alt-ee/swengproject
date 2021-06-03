34505f2330af6b1d5da476f6ad689ea973d66927
Tom E <tse503@york.ac.uk>
2021-06-03 14:47:04 +0100

    Improved method of determining size of bounds in text handler, now using FontMetrics to derive appropriate heights and widths for JLabel

--- src/main/java/survivalapp/SurvivalApp.java
+++ src/main/java/survivalapp/SurvivalApp.java
@@ -1,12 +1,12 @@
 package survivalapp;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-//import datastorage.Slideshow;
 import datastorage.Slideshow;
 import org.xml.sax.SAXException;
 import parser.*;
@@ -18,13 +18,13 @@ import javax.xml.parsers.ParserConfigurationException;
 public class SurvivalApp {
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, URISyntaxException, LineUnavailableException, UnsupportedAudioFileException {
-        File xmlFile = new File("src/main/resources/firstIterationSlide.xml");
+        File xmlFile = new File("src/main/resources/birds.xml");
 
         Parser parser = new Parser();
         Slideshow slideshow = parser.parse(xmlFile);
 
         Controller cont = new Controller(slideshow);
-        slideshow.setCurrentSlide("01");
+        slideshow.setCurrentSlide("Chaffinch");
         cont.drawCurrentSlide();
     }
 
