832cbf2787328b9b4eb78a92a9b09bba411faddb
at1468 <59831659+at1468@users.noreply.github.com>
2021-06-04 19:01:00 +0100

    Wrote text button test

---
+++ src/test/java/buttonhandler/ButtonHandlerManualTest.java
@@ -0,0 +1,36 @@
+package buttonhandler;
+
+import junit.framework.TestListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonHandlerManualTest {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(null);
+        frame.add(panel);
+
+        ButtonHandler buttonHandler = new ButtonHandler(panel);
+
+        frame.setVisible(true);
+
+        buttonHandler.addTextButton(100, 100, 100, 20, "testid", "testButton", "Calibri", 14, Color.BLUE, new TestListener());
+
+        panel.repaint();
+    }
+
+    static class TestListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+        }
+    }
+}
