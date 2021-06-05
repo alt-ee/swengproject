1801f348e57810c411f22ffef6fd68806f72ce6c
Tom E <tse503@york.ac.uk>
2021-06-05 15:43:41 +0100

    Code to generate waveform graphics from wav file

---
+++ src/main/java/waveformmaker/ReadWAV.java
@@ -0,0 +1,87 @@
+package waveformmaker;
+
+import graphicshandler.GraphicsPanel;
+import wavreading.WavFile;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.Arrays;
+
+public class ReadWAV
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			// Open the wav file specified as the first argument
+			WavFile wavFile = WavFile.openWavFile(new File("src/main/resources/RedKite1.wav"));
+
+			// Get the number of audio channels in the wav file
+			int numChannels = wavFile.getNumChannels();
+
+			// Get the number of audio samples (frames) in the wav file - 1 frame can have multiple channels
+			double numFrames = wavFile.getNumFrames();
+
+			//Width of the waveform graphic (duration) in pixels, essentially the number of lines used to represent the waveform
+			int waveformWidth = 360;
+
+			//Height of the waveform graphic (max amplitude) in pixels
+			int waveformHeight = 100;
+
+			System.out.println("Frames: " + numFrames);
+
+			int bufferSize = (int) Math.ceil(numFrames/waveformWidth);
+			System.out.println("Buffer size: " + bufferSize);
+
+			// Create a buffer with number of frames determined by bufferSize
+			double[] buffer = new double[bufferSize * numChannels];
+
+			//Store the max sample from each buffer
+			double[] maxSample = new double[waveformWidth];
+
+			int framesRead;
+			double max = Double.MIN_VALUE;
+
+			int i = 0;
+			do {
+				// Read frames into buffer
+				framesRead = wavFile.readFrames(buffer, bufferSize);
+				System.out.println("Frames read: " + framesRead + " in run: " + i);
+
+				// Loop through frames and look for minimum and maximum value
+				for (int j = 0 ; j < framesRead * numChannels ; j++) {
+					if (buffer[j] > max) {
+						max = buffer[j];
+					}
+				}
+
+				maxSample[i] = max;
+
+				max = Double.MIN_VALUE;
+				i++;
+			}
+			while (framesRead == bufferSize);
+
+			JFrame f = new JFrame();                            //Create new JFrame, acting as the main window
+			GraphicsPanel gp = new GraphicsPanel();
+			f.setVisible(true);                                 //Set the frame to visible
+			f.setSize(1600, 900);                   //Set size to 1600x900 pix
+			f.setTitle("Graphics Demo");                        //Set frame title to "Graphics Test"
+			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //Will exit out when close button pressed
+			f.add(gp);                                          //Add the GraphicsPanel object to the frame
+
+			for (int s = 0; s < waveformWidth; s++) {
+				gp.addLine(100 + s, 200 + (int)(waveformHeight * maxSample[s])/2, 100 + s, 200 - (int)(waveformHeight * maxSample[s])/2, "#b0b3b8", 0);
+			}
+			gp.repaint();
+
+
+			// Close the wavFile
+			wavFile.close();
+		}
+
+		catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+}
