54b3af81b1796ed1817b1bced553dd88d841e58e
at1468 <59831659+at1468@users.noreply.github.com>
2021-06-05 13:24:38 +0100

    Wrote addImageButton to pass test

--- src/main/java/buttonhandler/ButtonHandler.java
+++ src/main/java/buttonhandler/ButtonHandler.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.EventListener;
 
 public class ButtonHandler {
@@ -22,6 +23,18 @@ public class ButtonHandler {
         button.setBounds(xPos, yPos, width, height);
         button.putClientProperty("targetid", id);
         button.addActionListener(listener);
+
+        panel.add(button);
+    }
+
+    public void addImageButton(int xPos, int yPos, int width, int height, String id, String imageLocation, ActionListener listener) {
+        Image buttonIconImage = new ImageIcon(imageLocation).getImage();
+        Icon buttonIconScaled = new ImageIcon(buttonIconImage.getScaledInstance(width, height, Image.SCALE_SMOOTH));
+
+        JButton button = new JButton(buttonIconScaled);
+        button.putClientProperty("targetid", id);
+        button.addActionListener(listener);
+        button.setBounds(xPos, yPos, width, height);
 
         panel.add(button);
     }
