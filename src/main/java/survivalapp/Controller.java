5a81fc51f21c1aa2d2d7b371bde6bd980d16bf8e
at1468 <59831659+at1468@users.noreply.github.com>
2021-06-06 11:40:38 +0100

    Wrote SlideListener to pass test

--- src/main/java/survivalapp/Controller.java
+++ src/main/java/survivalapp/Controller.java
@@ -4,7 +4,10 @@ import datastorage.*;
 import org.xml.sax.SAXException;
 import parser.Parser;
 
+import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
@@ -13,15 +16,23 @@ public class Controller {
     private Slideshow slideshow;
     private View view;
 
-    // Temporary constructor for testing
+    // Temporary constructors for testing
     public Controller(File slideshowFile) throws ParserConfigurationException, SAXException, IOException {
         slideshow = Parser.parse(slideshowFile);
 
-        view = new View();
+        view = new View(new SlideListener(), new MediaListener());
         view.newWindow(390, 844);
     }
 
+    public Controller(Slideshow slideshow) {
+        this.slideshow = slideshow;
+
+        view = new View(new SlideListener(), new MediaListener());
+        view.newWindow(1280, 720);
+    }
+
     public void drawCurrentSlide() {
+        view.clearPanel();
         SlideDataStorage slide = slideshow.getCurrentSlide();
 
         drawAllImages(slide);
@@ -30,6 +41,7 @@ public class Controller {
         drawAllLines(slide);
         drawAllVideos(slide);
         setupAudio(slide);
+        drawAllButtons(slide);
 
         view.repaintPanel();
     }
@@ -97,6 +109,37 @@ public class Controller {
 
         }
 
+    }
+
+    private void drawAllButtons(SlideDataStorage slide) {
+        if (slide.hasButton()) {
+            Iterator<ButtonDataStorage> buttonIter = slide.buttonIterator();
+
+            while (buttonIter.hasNext()) {
+                view.drawButton(buttonIter.next());
+            }
+        }
+    }
+
+    private class SlideListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            String id = ((String)((JButton)ae.getSource()).getClientProperty("targetid"));
+
+            slideshow.setCurrentSlide(id);
+            drawCurrentSlide();
+        }
+    }
+
+    private class MediaListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            String id = ((String)((JButton)ae.getSource()).getClientProperty("targetid"));
+
+            System.out.println("MediaListener called with targetid: " + id);
+        }
     }
 
 }
