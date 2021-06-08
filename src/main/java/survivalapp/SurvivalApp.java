6d7b113156c71eec0bdc76634d31e55a22159956
Bosco Wang <cw1894@york.ac.uk>
2021-06-08 01:34:03 +0100

    add FileChooser

--- src/main/java/survivalapp/SurvivalApp.java
+++ src/main/java/survivalapp/SurvivalApp.java
@@ -16,9 +16,15 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.xml.parsers.ParserConfigurationException;
 
 public class SurvivalApp {
+    private static final String resourcesPath = "src/main/resources/";
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, URISyntaxException, LineUnavailableException, UnsupportedAudioFileException {
-        File xmlFile = new File("src/main/resources/birds.xml");
+        FileChooser chooser = new FileChooser();
+        chooser.openDialog(resourcesPath);
+
+        File xmlFile = new File(chooser.hadChoseFile()?
+                chooser.getChoseFilePath() :
+                resourcesPath + "birds.xml");
 
         Controller controller = new Controller(xmlFile);
         controller.drawCurrentSlide();
